6b4bf8ddd87a65df06d48e6348ecf50868602b38
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-07 13:33:15 +0300

    Create class for the punish inventory

---
+++ src/main/java/me/gregzee/xpunishmentgui/inventory/PunishInventory.java
@@ -0,0 +1,17 @@
+package me.gregzee.xpunishmentgui.inventory;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class PunishInventory implements Listener {
+
+	/**
+	 * Opens the punish inventory for the specified player.
+	 *
+	 * @param player the player to open the inventory for
+	 * @param target the player to punish
+	 */
+	public void openPunishInventory(Player player, Player target) {
+		// logic here
+	}
+}
