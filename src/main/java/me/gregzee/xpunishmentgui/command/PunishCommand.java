812a899d071f3f48f3d017ad6463f6304ba81683
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-07 13:22:51 +0300

    Create PunishCommand structure

--- src/main/java/me/gregzee/xpunishmentgui/command/PunishCommand.java
+++ src/main/java/me/gregzee/xpunishmentgui/command/PunishCommand.java
@@ -1,4 +1,18 @@
 package me.gregzee.xpunishmentgui.command;
 
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+
 public class PunishCommand {
+
+	public static void register() {
+
+		new CommandAPICommand("punish")
+			.withPermission("xpunishmentgui.punish")
+			.withArguments(new PlayerArgument("target"))
+			.withUsage("/punish <target>")
+			.executesPlayer((player, args) -> {
+				// logic here?
+			});
+	}
 }
