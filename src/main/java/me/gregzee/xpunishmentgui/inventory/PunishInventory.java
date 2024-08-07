a885cbf9b9eb5f0d8ba170cf4d11a1ba9bdcfee0
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-07 13:43:33 +0300

    Added the createPunishInventory method, implemented InventoryHolder, removed the Listener, added a getter for the inventory, added a field for the inventory and added a constructor

--- src/main/java/me/gregzee/xpunishmentgui/inventory/PunishInventory.java
+++ src/main/java/me/gregzee/xpunishmentgui/inventory/PunishInventory.java
@@ -1,17 +1,36 @@
 package me.gregzee.xpunishmentgui.inventory;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.jetbrains.annotations.NotNull;
 
-public class PunishInventory implements Listener {
+public class PunishInventory implements InventoryHolder {
+
+	private final Inventory inventory;
+
+	public PunishInventory(Player player, Player target) {
+		this.inventory = createPunishInventory(player, target);
+	}
 
 	/**
-	 * Opens the punish inventory for the specified player.
+	 * Creates the punish inventory for the specified player with the target specified.
 	 *
 	 * @param player the player to open the inventory for
 	 * @param target the player to punish
 	 */
-	public void openPunishInventory(Player player, Player target) {
+	public Inventory createPunishInventory(Player player, Player target) {
 		// logic here
+
+		return null;
+	}
+
+	/**
+	 * Getter for the punish inventory.
+	 * @return the inventory
+	 */
+	@Override
+	public @NotNull Inventory getInventory() {
+		return this.inventory;
 	}
 }
