177349c9a0217db2c8ba62a553a787f2bde12b7a
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-10 15:47:43 +0300

    Create InventoryClickListener

---
+++ src/main/java/me/gregzee/xpunishmentgui/listener/InventoryClickListener.java
@@ -0,0 +1,29 @@
+package me.gregzee.xpunishmentgui.listener;
+
+import me.gregzee.xpunishmentgui.inventory.PunishInventory;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class InventoryClickListener implements Listener {
+
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event) {
+		Inventory inventory = event.getClickedInventory();
+
+		if (inventory == null || !(inventory.getHolder(false) instanceof PunishInventory punishInventory)) {
+			return;
+		}
+
+		event.setCancelled(true);
+
+		ItemStack clickedItem = event.getCurrentItem();
+
+		if (clickedItem != null && clickedItem.getType() == Material.BARRIER) {
+
+		}
+	}
+}
