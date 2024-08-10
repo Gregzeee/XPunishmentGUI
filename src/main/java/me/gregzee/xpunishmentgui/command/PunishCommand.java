6f4aa713562b8e137532e7fbfe1b269d6dad7995
Gregzee <95419312+Gregzeee@users.noreply.github.com>
2024-08-10 15:47:33 +0300

    Added command logic and registered the command

--- src/main/java/me/gregzee/xpunishmentgui/command/PunishCommand.java
+++ src/main/java/me/gregzee/xpunishmentgui/command/PunishCommand.java
@@ -2,17 +2,24 @@ package me.gregzee.xpunishmentgui.command;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.PlayerArgument;
+import me.gregzee.xpunishmentgui.inventory.PunishInventory;
+import org.bukkit.entity.Player;
 
 public class PunishCommand {
 
-	public static void register() {
+	public PunishCommand() {
+		registerPunishCommand();
+	}
+
+	public static void registerPunishCommand() {
 
 		new CommandAPICommand("punish")
 			.withPermission("xpunishmentgui.punish")
 			.withArguments(new PlayerArgument("target"))
 			.withUsage("/punish <target>")
 			.executesPlayer((player, args) -> {
-				// logic here
-			});
+				PunishInventory punishInventory = new PunishInventory((Player) args.get("target"));
+				player.openInventory(punishInventory.getInventory());
+			}).register();
 	}
 }
